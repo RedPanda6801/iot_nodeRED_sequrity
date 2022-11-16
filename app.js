564b3f7cbe04c217b55606ad4e43c4f1450579fe
mickeyhunu <mickeyhunu@gmail.com>
2022-11-16 15:16:37 +0900

    verson1
    프론트 수정

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const mainRouter = require("./routes/main");
 const app = express();
 app.set("port", process.env.PORT || 8002);
 app.use(morgan("dev"));
-app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
@@ -24,6 +24,7 @@ app.use("/", mainRouter);
 app.listen(app.get("port"), () => {
   console.log(app.get("port"), "번 포트에서 대기중");
 });
+
 
 // 라즈베리파이에서 버튼을 누르면 사진을 찍어 저장
 // 찍은 사진을 보내서 서버에 이미지로 저장
