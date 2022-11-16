436e67c71fe446b9bbb0770c9aa606c6bbef5e38
박승규 <redpanda6801@gmail.com>
2022-11-16 09:53:26 +0900

    프론트 수정을 위한 커밋

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const express = require("express");
+const morgan = require("morgan");
 const path = require("path");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const mqtt = require("mqtt");
+dotenv.config();
+
 // 라우터 불러오기
 const mainRouter = require("./routes/main");
 const app = express();
 app.set("port", process.env.PORT || 8002);
+app.use(morgan("dev"));
 app.use("/", express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(
+  cors({
+    credentials: true,
+  })
+);
 // 라우터 설정
 app.use("/", mainRouter);
-// 404 NOT FOUND
 app.listen(app.get("port"), () => {
   console.log(app.get("port"), "번 포트에서 대기중");
 });
+
+// 라즈베리파이에서 버튼을 누르면 사진을 찍어 저장
+// 찍은 사진을 보내서 서버에 이미지로 저장
+// 프론트에서 버튼을 누르면 라즈베리로 데이터를 전송
+// 데이터를 확인하여 LED를 켜줌
--- routes/main.js
+++ routes/main.js
@@ -1,13 +1,36 @@
 const express = require("express");
 const path = require("path");
 const router = express.Router();
+const mqtt = require("mqtt");
+const fs = require("fs");
 
 router.get("/", async (req, res) => {
   try {
+    const client = mqtt.connect("mqtt://192.168.0.122");
+    client.on("connect", function () {
+      console.log("Connection Success");
+      client.subscribe("topic", function (err) {
+        if (!err) {
+        }
+      });
+      client.publish("btn", "close");
+    });
+    client.on("message", function (topic, message) {
+      // message is Buffer
+      console.log(message);
+      fs.writeFileSync("uploads/new-path.jpg", message);
+    });
     return res.sendFile(path.join(__dirname, "../views/phone.html"));
-  } catch (err) {
-    console.log(err);
+  } catch (error) {
+    console.log(error);
   }
 });
+// router.post("/btn", async (req, res) => {
+//   try {
+//     console.log(req.body);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 
 module.exports = router;
